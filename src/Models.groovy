d770b9e5c104858b9fb82a8ea19579b3f8dad34c
ksprakash <ksaiprakash01@gmail.com>
2022-03-31 15:58:59 -0400

    Create Models.groovy

---
+++ src/Models.groovy
@@ -0,0 +1,16 @@
+class Models {
+    String name;
+    int age;
+    boolean isHuman;
+
+    Models(String name,int age,isHuman){
+        this.name=name;
+        this.age = age;
+        this.isHuman = isHuman;
+    }
+
+    String Capitalize(String name){
+        return name.capitalize();
+
+    }
+}
